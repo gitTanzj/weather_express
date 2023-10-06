487c83019a326a236f880fe48261bf49e5486462
gitTanzj <kalle.riit@gmail.com>
2023-10-06 10:56:52 +0300

    Created form request response with API data

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require('path')
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 const key = '77fefff6fbdbd270c709438fbdfa6295'
 let city = 'Tartu'
 
@@ -25,6 +28,23 @@ app.get('/', (req, res) => {
         })
     })
     
+})
+
+app.post('/', (req,res) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.body.cityname}&appid=${key}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp)-273.15)
+        res.render('default.ejs', {
+            description : description,
+            city : city,
+            temp : temp
+        })
+    })
 })
 
 app.listen(3000)
